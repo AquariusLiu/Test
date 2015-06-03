1b46c23fedfceecaea92b1ac8dfd0c2050dcd441
Liu XiangYu <liuxiangyu@pb6a80205.nigtnt01.ap.so-net.ne.jp>
2015-06-03 23:37:36 +0800

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Liu XiangYu on 15/6/3.
+//  Copyright (c) 2015年 Liu XiangYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Liu XiangYu on 15/6/3.
+//  Copyright (c) 2015年 Liu XiangYu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
